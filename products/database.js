d7efd8b098df31c767adb7b305abb85a980d8c22
PitchforkEmporium <PitchforkEmporiumCom@gmail.com>
2016-05-12 21:02:21 +0200

    Create database.js

---
+++ products/database.js
@@ -0,0 +1,38 @@
+var pitchforkBase=[
+  "--|-[",
+  "",
+  "",
+  "",
+  "",
+  ""
+];
+
+var nameBase=[
+  "PicturElements Special",
+  "",
+  "",
+  "",
+  "",
+  ""
+];
+
+var descBase=[
+  "MUH PITCHFORK",
+  "",
+  "",
+  "",
+  "",
+  ""
+];
+
+function getPitchfork(inp){
+  return pitchforkBase[inp];
+}
+
+function getItemName(inp){
+  return nameBase[inp];
+}
+
+function getItemDesc(inp){
+  return descBase[inp];
+}
